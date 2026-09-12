33621faacbcc2a0e73963f0eaf6d6a6ab799af3d
Janita Omdahl Reiraa <janitaomdahlreiraa@gmail.com>
2026-09-12 15:21:15 +0200

    card CSS

--- Video-poker/src/Components/Buttons/DealOrDrawBtn/DealOrDrawBtn.tsx
+++ Video-poker/src/Components/Buttons/DealOrDrawBtn/DealOrDrawBtn.tsx
@@ -1,8 +1,11 @@
 import "../btn.css";
 
 export default function DealOrDrawBtn() {
+  function alertMe() {
+    alert("deal er trykket");
+  }
   return (
-    <button>
+    <button onClick={alertMe}>
       <h2>Deal or Draw</h2>
     </button>
   );
--- Video-poker/src/Components/PayoutTable/PayoutTable.tsx
+++ Video-poker/src/Components/PayoutTable/PayoutTable.tsx
@@ -1,4 +1,6 @@
 export default function PayoutTable() {
+  const cardHandsReward = {};
+
   return (
     <div>
       <h2>PayoutTable</h2>
