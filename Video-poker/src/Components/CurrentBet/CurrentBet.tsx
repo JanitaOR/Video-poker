47f2f72927b58aeadc8ebed99a8cf320b60e0966
Janita Omdahl Reiraa <janitaomdahlreiraa@gmail.com>
2026-09-17 18:51:59 +0200

    tried to start on the store and begin on current bet value

--- Video-poker/src/Components/CurrentBet/CurrentBet.tsx
+++ Video-poker/src/Components/CurrentBet/CurrentBet.tsx
@@ -1,3 +1,6 @@
+import { useBet } from "../../store/store";
+
 export default function CurrentBet() {
-  return <h3>Current Bet: 1</h3>;
+  const bet = useBet();
+  return <h3>Current Bet: {bet}</h3>;
 }
--- Video-poker/src/store/store.ts
+++ Video-poker/src/store/store.ts
@@ -4,8 +4,8 @@ type Bet = {
   startBet: number;
 };
 
-export const useBet = create((set) => ({
-  bet: 1,
+export const useBet: Bet = create((set) => ({
+  startBet: 1,
   incrementOne: () => set((state: Bet) => ({ bet: state.startBet + 1 })),
   setBet: () => set((state: Bet) => ({ bet: (state.startBet = 5) })),
 }));
